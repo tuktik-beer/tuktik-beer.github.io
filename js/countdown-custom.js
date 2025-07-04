56f1d56babb3b62edfb7c6f5e33c6b4e5af8aa20
Nattanon Saetan <nattanon@hospital-os.com>
2025-07-04 21:40:44 +0700

    version 2

--- js/countdown-custom.js
+++ js/countdown-custom.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function () {
   jQuery(function () {
-    jQuery("#defaultCountdown").countdown({ until: new Date(2025, 8, 7) }); // year, month, date, hour
+    jQuery("#defaultCountdown").countdown({ until: new Date(2025, 7, 7) }); // year, month, date, hour
   });
 });
